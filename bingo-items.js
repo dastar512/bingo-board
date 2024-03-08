39fea27bc570940cd146ded3bac74a059ad37ba4
dastar512 <39785314+dastar512@users.noreply.github.com>
2024-03-08 15:43:54 -0500

    Update bingo-items.js
    first draft of bingo items list for fifi

--- bingo-items.js
+++ bingo-items.js
@@ -1,13 +1,30 @@
 var bingoItems = [
-    // Add bingo items to this list. If you must use the double quote character (") in an item, escape it like this: \"
+// Add bingo items to this list. If you must use the double quote character (") in an item, escape it like this: \"
     "Mic peak",
-    "Chat posts fifiMikes",
-    "Chat does groupStripp",
+    "Chat posts fifiMike pyramid", 
+    "Chat posts groupStripp pyramid",
     "Fifi says OH SHOOT",
     "Fifi has to poop",
-    "Somebody drops 50 gifted",
+    "Somebody drops 10 gifted",
     "Strimmer yells for mod attention",
     "Accidental Swear",
-    "Test ITEM 2"
+    "fifiDinkDonk announcement",
+    "Fifi thinks about eating ice cweam", 
+    "Webby sits in Fifi’s lap",
+    “Chat demands Fifi play a specific game”,
+    //“Bot spam for buying followers/subs”,
+    “Damn RAT”,
+    “Fifi says What is this lobby”,
+    “Fifi says That guy’s cheating”,
+    “Fifi says DUDE”,
+    “A Kiss on the Lips”, 
+    “Fifi says FRICK”,
+    “Fifi says HUHH”,
+    “Tempted to open cards”,
+    “Suggests sshrk or linkfan21 get banned”,
+    “Fifi says linkydinky”,
+    “Fifi bleeps out curse words”,
+    “Hates on Overwatch”,
+    “Chauncey is unhelpful”
     // For compatibility reasons, the very last item in the list should not have a comma after it.
 ];
