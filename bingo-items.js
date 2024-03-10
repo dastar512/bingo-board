6e89ccb57a69ef9a2da0ce01710a3f9fbd837472
dastar512 <39785314+dastar512@users.noreply.github.com>
2024-03-09 22:30:07 -0500

    Update bingo-items.js
    added items, fixed quotation marks

--- bingo-items.js
+++ bingo-items.js
@@ -25,18 +25,19 @@ var bingoItems = [
     "Fifi bleeps out curse words",
     "Hates on Overwatch",
     "Chauncey is unhelpful",
-    “Questionable Innuendo”,
-    “Fifi flashbangs chat with light mode”,
-    “Bad teammate in PvP game”,
-    “Fifi says WOOOOOOW”,
-    //“Fifi says GOOOOOOD MOOORNING TO THE CHAAAAT”, (free)
-    “Fifi calls someone a cutie”,
-    “Fifi gets a coffee”,
-    “Fifi says Ashes shut up”,
-    “Fifi says Let’s freakin go”,
-    “Fifi says I love this game”,
-    “Fifi says I hate this game”,
-    “Fifi says nutsack”,
-    “Fifi apologizes for something”,
+    "Questionable Innuendo",
+    "Fifi flashbangs chat with light mode",
+    "Bad teammate in PvP game",
+    "Fifi says WOOOOOOW",
+    //"Fifi says GOOOOOOD MOOORNING TO THE CHAAAAT”, (free)
+    "Fifi calls someone a cutie",
+    "Fifi gets a coffee",
+    "Fifi says Ashes shut up",
+    "Fifi says Let’s freakin go",
+    "Fifi says I love this game",
+    "Fifi says I hate this game",
+    "Fifi says nutsack",
+    "Fifi apologizes for something",
+    "Fifi pulls a chase card"
     // For compatibility reasons, the very last item in the list should not have a comma after it.
 ];
